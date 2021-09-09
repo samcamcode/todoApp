22abb617e39c18f2f223f592623c2ee0f5e0682b
Samuel Camacho <samcamcode@yahoo.com>
2021-09-08 22:30:47 -0700

    made changes to styling in todo, added edit button and event listner

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const addBtn = document.querySelector('.add-btn');
+const editBtn = document.querySelector('.edit-btn');
 const deleteBtn = document.querySelector('.delete-btn');
 const input = document.querySelector('#todo');
 const list = document.querySelector('.list');
 const span = document.querySelector('span');
 
 const createTodo = (value) => {
-    return `<div class="each-todo">
+    return `
     <span class="todo-item">${value}</span>
-    <button class="delete-btn">Delete</button>
-    </div>`
+    <input type="button" class="todo-btn edit-todo" value="Edit">
+    <input type="button" class="todo-btn delete-btn" value="Delete">`
 }
 
+//event listener for add todo
 addBtn.addEventListener('click', () => {
     if (input.value === '') return
     const div = document.createElement('div')
-    div.classList.add('todos-list')
+    div.classList.add('each-todo')
     div.innerHTML = createTodo(input.value)
     list.appendChild(div)
     input.value = '';
 })
 
-// dynamic event listener for deletebtn
+//event listener for edit todo
+editBtn.addEventListener('click', () => {
+    const editTodo = document.getElementsByClassName('editing')[0];
+    editTodo.childNodes[1].innerText = input.value;
+    editTodo.classList.remove('editing');
+    input.value = '';
+})
+
+// dynamic event listener for delete-btn
 list.addEventListener('click', (e) => {
     if (e.target.classList.contains('delete-btn')){
         e.target.parentElement.remove()
@@ -34,4 +44,16 @@ list.addEventListener('click', (e) => {
     }
 })
 
+// dynamic event listener for edit-todo
+list.addEventListener('click', (e) => {
+    if (e.target.classList.contains('edit-todo')){
+        e.target.parentElement.classList.add('editing')
+        input.value = e.target.parentElement.innerText;
+    }
+})
 
+// changes
+// 2 sep
+// add todo btn changed to input type button
+// changed styling from each-todo to todo-item
+// working on edit-todo creating btn and editing todo with changing the entire element
