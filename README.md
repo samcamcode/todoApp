3baa141228f325f37db375acdab39404a4f443a9
sam <sam@pop-os.localdomain>
2021-09-19 01:55:31 -0700

    Created a checkbox switch to toggle dark/light mode on checkbox being checked

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Second commit - Styling changes, event listeners for persistent and dynamic elem
 Third commit - Styling, card like look on the container aslo on each todo item. Hiding element when not needed.
 
 Fourth commit - Fixed issues with edge cases in editing. Added a default task so the user can see how the tasks are laid out.
+
+Fifth commit - Created a checkbox switch to toggle dark/light mode on checkbox being checked 
--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+const allBtns = document.querySelectorAll('.btn');
 const addBtn = document.querySelector('.add-btn');
 const editBtn = document.querySelector('.edit-btn');
+const editTaskBtn = document.querySelector('.edit-todo');
 const deleteBtn = document.querySelector('.delete-todo');
 const input = document.querySelector('#todo');
 const list = document.querySelector('.list');
 const span = document.querySelector('span');
+const container = document.querySelector('.container');
+const lightSwitch = document.querySelector('#switch');
 
 const createTodo = (value) => {
     return `
     <span class="todo-item">${value}</span>
     <input type="button" class="btn todo-btn edit-todo" value="Edit">
     <input type="button" class="btn todo-btn delete-todo" value="Delete">`
+}
+
+// change elements from light to dark
+const darkmode = (bool) => {
+    document.body.style.backgroundColor = bool ? '#121212' : '#4267b2';
+    document.body.style.color = bool ? '#e4e6eb' : '#121212';
+    container.style.backgroundColor = bool ? '#171f24' : '#f5f5f5';
+    container.style.borderColor = bool ? '#121212' : '#f5f5f5';
+    allBtns.forEach(btn => {
+        btn.style.color = bool ? '#121212': '#f5f5f5';
+    })
 }
 
 // toggle disable class on each task that is not being edited
@@ -24,6 +39,14 @@ const toggleDisableClass = () => {
     }
 } 
 
+// check to see if switch is checked after restarting the page
+darkmode(lightSwitch.checked)
+
+// toggle between light and dark
+lightSwitch.addEventListener('click', () => {
+    darkmode(lightSwitch.checked)
+})
+
 //event listener for adding a task
 addBtn.addEventListener('click', () => {
     if (input.value === '') return
@@ -32,10 +55,6 @@ addBtn.addEventListener('click', () => {
     div.innerHTML = createTodo(input.value)
     list.appendChild(div)
     input.value = '';
-    const defaultTask = document.getElementsByClassName('default')[0];
-    setTimeout(() =>{
-        defaultTask.classList.add('hide');
-    },250)
 })
 
 //event listener for editing task
